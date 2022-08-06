3cfbe96d2462f5ca2556ad62a6e4b775b60be0fa
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-06 18:21:24 +0530

    Create findinteger.java

---
+++ findinteger.java
@@ -0,0 +1,31 @@
+Given a number find given number is Integer or not.
+
+
+
+
+import java.io.*;
+import java.util.*;
+public class MyClass
+{
+  public static void main (String [] args)
+   {
+     Scanner sc = new Scanner(System.in);
+     double d = sc.nextDouble();
+     int x =(int)d;
+     if(d-x==0) 
+     System.out.println("this Integer");
+     else 
+     System.out.println("this Not Integer");
+     
+  
+  
+  
+      
+
+      
+    }
+  }
+
+                 
+                 
+                 
