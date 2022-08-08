ce114894a06f8451b8519ccfd025698f7812d16f
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-08 17:18:57 +0530

    Create Associativity.java

---
+++ Associativity.java
@@ -0,0 +1,12 @@
+public class MyClass
+{
+  public static void main (String [] args){
+    //Quick  Quiz
+    int b=1;
+    int c=4;
+    int a=5;
+    int k= b*b-(4*a*c)/(2*a);
+    System.out.println(k);
+      
+ }    
+}
