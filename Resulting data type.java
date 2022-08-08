05a3b826c555f5af08aeecabb86def9f0f0044fe
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-08 17:24:53 +0530

    Create Resulting data type.java

---
+++ Resulting data type.java
@@ -0,0 +1,13 @@
+public class MyClass
+{
+  public static void main (String [] args){
+    byte x=5;
+    short z=8;
+    int y =6;
+    int a=y+z;
+    float b= 6.54f+x;
+    System.out.println(b);
+  
+      
+ }    
+}
