98437b4765b2240cfda83b50ade18ed36ba5d971
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-08 11:04:36 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,21 @@
+public class MyClass
+{
+  public static void main (String [] args){
+     // 1. Arithmetic operators
+      // int a=4;
+     // int b=6%a;
+      //System.out.println(b);
+      
+     // 2. Assignment operator
+      // int b=9;
+    // b*=3;
+    // System.out.println(b);
+    
+    // 3. Comparison operator
+    // System.out.println(50<5);
+   
+    // 4. Logical operators
+     //System.out.println(64>5&&64>98);
+     
+    // 5.Bitwise operators 
+    System.out.println(263);
