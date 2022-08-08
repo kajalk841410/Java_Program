fb53bbd19e7b340ca0989b1de27270b9a2063a5a
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-08 17:28:38 +0530

    Create Increment and decrement operators.java

---
+++ Increment and decrement operators.java
@@ -0,0 +1,14 @@
+public class MyClass
+{
+  public static void main (String [] args){
+    // Increment and decrement operators
+    int i=56;
+    System.out.println(i++);
+    System.out.println(i);
+    System.out.println(++i);
+    System.out.println(i);
+    
+  
+      
+ }    
+}
