1c5df980a1deb86563a2612f697c17a94c333cd1
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-09 09:40:36 +0530

    Create Decrypting the grade.java

---
+++ Decrypting the grade.java
@@ -0,0 +1,15 @@
+public class MyClass
+{
+  public static void main (String [] args){
+    //write a java program to encriypt q grade by adding 8 to it.
+    char grade ='B';
+    grade = (char) (grade+8);
+    System.out.println(grade);
+    //Decrypting the grade
+    grade = (char)(grade-8);
+    System.out.println(grade);
+    
+  
+      
+ }    
+}
