cf9506a2aac974a4a000853fc528897b056f6524
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-09 10:15:57 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,10 @@
+public class Myclass{
+public static void main (String [] args){
+     String name = new String ("Kajal");
+    // String name =new String "Kajal";
+     System.out.println("the name is :");
+     System.out.println(name);
+        
+      
+ }    
+}
