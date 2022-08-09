71b7f41280b37b8221859e6b713c0196c2761f77
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-09 10:21:43 +0530

    Create String length.java

---
+++ String length.java
@@ -0,0 +1,11 @@
+public class Myclass{
+public static void main (String [] args){
+     String name = "Kajal";
+    // System.out.println(name);
+    int value = name.length();
+    System.out.println(value);
+     
+     
+      
+ }    
+}
