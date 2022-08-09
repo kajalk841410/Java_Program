2965b292427a6569323d708b155503c390a9b272
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-09 11:33:40 +0530

    Create double and triple spaces in a string.java

---
+++ double and triple spaces in a string.java
@@ -0,0 +1,11 @@
+public class Myclass{
+public static void main (String [] args){
+    // write a java program to detect double and triple spaces in a string.
+     String mystring = "this string contains double and triple space";
+     System.out.println(mystring.indexOf("  "));
+     System.out.println(mystring.indexOf("   "));
+     
+ 
+      
+ }    
+}
