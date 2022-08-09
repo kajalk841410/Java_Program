d60becad2edc55a16df4d1b328fef807ad19f7d9
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-09 10:08:40 +0530

    Create Given number is grater than number or not.java

---
+++ Given number is grater than number or not.java
@@ -0,0 +1,12 @@
+import java.util. Scanner;
+public class Myclass{
+
+  public static void main (String [] args){
+      // use comparison operators to find out weather a given number is greater than the user entered number or not
+    Scanner sc=new Scanner (System.in);
+    int a=sc.nextInt();
+    System.out.println(a>8);
+        
+      
+ }    
+}
