0b30c0f92f1f504108a08aacebe0de2fedb2dcf0
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-10 12:49:40 +0530

    Create Conditionals 1.java

---
+++ Conditionals 1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Myclass{
+public static void main (String [] args){
+    int age;
+    System.out.println("enter your age");
+    Scanner sc = new Scanner(System.in);
+    age = sc.nextInt();
+    if (age>56){
+        System.out.println("you are experienced!");
+    }
+    else if(age>46){
+        System.out.println("you are semi- experienced!");
+        
+    }
+    else if (age>36){
+        
+   System.out.println("you are semi- semi experienced!"); 
+        
+    }
+    else{
+      System.out.println("you are not experienced!");  
+    }
+ }    
+}
