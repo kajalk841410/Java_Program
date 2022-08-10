de354c89ca143e5c8263278d83f9e83eafd81130
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-10 12:35:50 +0530

    Create Conditions.java

---
+++ Conditions.java
@@ -0,0 +1,12 @@
+public static void main (String [] args){
+    int age = 20;
+    if (age>18){
+    System.out.println("yes boy you can drive");
+    
+    }
+    else{
+        System.out.println("no boy you cannot drive yet");
+        
+    }
+ }    
+}
