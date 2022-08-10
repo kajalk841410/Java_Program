7512fdbcb86b17e9966534519014a81ba7fdbc3e
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-10 17:29:52 +0530

    Create Swichcase.java
     write a java program to find out the day of the week given the number

---
+++ Swichcase.java
@@ -0,0 +1,35 @@
+
+// write a java program to find out the day of the week given the number
+import java.io.*;
+import java.util.*;
+public class Myclass{
+public static void main (String [] args){
+     Scanner sc= new Scanner (System.in);
+        int day = sc.nextInt();
+        switch (day)
+        {
+        
+        case 0 : System.out.println("Monday"); break;
+        case 1 : System.out.println("Tuesday"); break;
+        case 2 : System.out.println("Wednesday");break;
+         case 3: System.out.println("Thursday"); break;
+         case 4:System.out.println("Friday"); break;
+        case 5 :System.out.println("Saturday");break;
+        case 6 : System.out.println("Sunday");break;
+        default : System.out.println("Invalid input");
+          }
+    
+        }
+
+}
+
+
+
+
+
+
+
+
+
+
+
