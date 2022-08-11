ea8318804bd54c7053e97979db26766fa87c9cd9
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-12 01:07:53 +0530

    Create do while loop1.java

---
+++ do while loop1.java
@@ -0,0 +1,14 @@
+  //Write a program to print first n natural numbar using do-while loops.
+  public class Myclass{
+    public static void main(String [] args){
+    int i=1;
+    do{
+        System.out.println(i);
+        i++;
+    }
+        while(i<=30);
+    
+        
+        
+    }
+}
