b231ab143303f0469ebd05d6ee20cccc51fecf2c
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-12 00:59:40 +0530

    Create do while loop.java

---
+++ do while loop.java
@@ -0,0 +1,16 @@
+public class Myclass{
+    public static void main(String [] args){
+        int a= 0;
+        while (a<5){
+            System.out.println(a);
+            a++;
+        }
+        int b=10;
+        do{
+            System.out.println(b);
+            b++;
+        }
+            while (b<5);
+        
+    }
+}
