de8c725c8976d2f2fa897f7ab9c200f146c0aeae
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-12 01:15:09 +0530

    Create for loop.java
    Write a program to print first n n odd number using a for loop.

---
+++ for loop.java
@@ -0,0 +1,13 @@
+//Write a program to print first n n odd number using a for loop.
+public class Myclass{
+    public static void main(String []args){
+        int n= 9;
+        for(int i=0;i<5;i++){
+            System.out.println(2*i+1);
+        
+    
+        }
+        
+        
+    }
+}
