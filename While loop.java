34a8ef93443337496b1fbddf1fb03978c2bb6d7c
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-12 00:53:20 +0530

    Create While loop.java

---
+++ While loop.java
@@ -0,0 +1,19 @@
+public class Myclass{
+    public static void main(String [] args){
+        System.out.println(1);
+         System.out.println(2);
+          System.out.println(3);
+          
+          System.out.println("using loops.");
+          int i=1;
+          while(i<3){
+              System.out.println(i);
+              i++;
+            
+              System.out.println("finish running while loops!");
+              while(true){
+                  System.out.println("i am an infinite while loopa!"); 
+              }  
+          }
+    }
+}
