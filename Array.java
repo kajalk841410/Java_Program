b1dac37d6b626c038db65e41b211ae0edb55987d
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-14 11:53:43 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Myclass{
+    public static void main(String[]args){
+        int [] Marks= new int [5];
+        
+    Marks [0]=100;
+    Marks [1]=60;
+    Marks [2]=50;
+    Marks [3]=80;
+    Marks [4]=90;
+    System.out.println(Marks[4]);
+    }
+    
+}
