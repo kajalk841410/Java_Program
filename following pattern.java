86bce08c031d2274cc1233152eafa5098d83b4e7
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-14 11:26:36 +0530

    Create following pattern.java
    Write a program to print the following pattern.

---
+++ following pattern.java
@@ -0,0 +1,17 @@
+  //Write a program to print the following pattern.
+   // ****
+   // ***
+    //**
+   // *
+    public class Myclass{
+    public static void main(String []args){
+    
+       int n=4;
+       for(int i=n;i>0;i--){
+        for(int j=0;j<i;j++){
+        System.out.println("*");    
+       }
+       System.out.println("\n");  
+       }
+    }
+}
