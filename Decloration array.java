2cd4c785d94fa16b190589ba5444a55ae7d46e46
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-14 12:00:21 +0530

    Create Decloration array.java

---
+++ Decloration array.java
@@ -0,0 +1,11 @@
+public class Myclass{
+    public static void main(String[]args){
+        // Decloration,memory allocation and intilization together .
+        int []Marks={98,45,79,99,88};
+        System.out.println(Marks[4]);
+    
+    
+    
+    
+    }
+}
