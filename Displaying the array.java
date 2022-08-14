6b0f81cecf456fee111da85268eba650c0cec6bb
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-14 12:06:41 +0530

    Create Displaying the array.java

---
+++ Displaying the array.java
@@ -0,0 +1,13 @@
+public class Myclass{
+    public static void main(String[]args){
+        // Displaying the array (for loop)
+        int [] marks={98,45,79,99,88};
+        for(int i=0;i<marks.length;i++){
+            System.out.println(marks[i]);
+        }
+    
+    
+    
+    
+    }
+}
