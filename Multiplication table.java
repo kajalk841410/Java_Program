5f7a4e34544d48892d4eae6291d1a8eb8b582630
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-14 11:38:59 +0530

    Create Multiplication table.java
    write a program to calculate the sum of the numbers acauring be the multiplication table of 8.

---
+++ Multiplication table.java
@@ -0,0 +1,12 @@
+// write a program to calculate the sum of the numbers acauring be the multiplication table of 8.
+
+public class Myclass{
+public static void main(String[] args){
+    int n= 8;
+    int sum =0;
+    for (int i=0;i<=2;i++){
+        sum +=n*1;
+    }
+    System.out.println(sum);
+}
+}
