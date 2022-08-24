dbb4bb86fc37a925124ee9f0cf77179dcd162d18
kajalk841410 <101002282+kajalk841410@users.noreply.github.com>
2022-08-24 22:40:32 +0530

    Create string litreal.java

---
+++ string litreal.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class A{
+	public static void main (String[] args) {
+		String a ="Kajal";//literal
+		System.out.println(a);
+		
+		String b ="Kajal";//literal
+		System.out.println(b);	
+		
+		a =a.concat("kumari");
+	 System.out.println(a);
+	}
+	
+}
